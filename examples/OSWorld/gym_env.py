5e7f6b016e04d8f215c6c8e2b83acc5c7e29f81c
natdosan <ndelrosx6@gmail.com>
2025-02-25 23:24:13 +0000

    update docstrings to align with Aaryan changes

--- examples/OSWorld/gym_env.py
+++ examples/OSWorld/gym_env.py
@@ -36,15 +36,26 @@ class StateGym(NamedTuple):
 
 class EnvironmentGym(Environment):
     """
-    WorldModel, but for gym environments. Instead of being based off of a textual example, takes in a gym environment. An LLM will not be used for generating new states. The gym environment's step function takes care of that.
+    WorldModel, but for gym environments. Instead of being based off of a 
+    textual example, takes in a gym environment. An LLM will not be used 
+    for generating new states. The gym environment's step function takes care of that.
 
-    Attributes:
-    - env (gym.Env): the gym environment
-    - example (dict): the benchmark example we are testing
-    - env_seed (int): the seed for the gym environment
-    - max_steps (int): the maximum number of steps that can be taken until is_terminal cuts off the episode
-    - obs_preprocessor (Optional[Callable[[dict], dict]]): optional function to process the observation returned from resetting/stepping the environment before it is stored into the state tuple
-    - env_current_obs (dict): the current observation of the environment which is used to check if a passed in state is aligned with the environment's current state
+    Attributes
+    -----------
+    env : DesktopEnv
+        OSWorld's environment object
+    example : dict
+        the benchmark example we are testing
+    env_seed : int
+        the seed for the gym environment (Default 42)
+    obs_preprocessor : Optional[Callable[[dict], dict]]
+        optional function to process the observation returned from 
+        resetting/stepping the environment before it is stored into the state tuple
+    env_current_obs : dict
+        the current observation of the environment which is used to check if 
+        a passed in state is aligned with the environment's current state
+    task_dir : str
+        directory where configs, logs stored
     """
 
     def __init__(
@@ -80,15 +91,28 @@ class EnvironmentGym(Environment):
 
     def step(self, state: StateGym, action: ActionGym) -> tuple[StateGym, dict]:
         """
-        Takes in a state and action and steps the environment. Should be noted that the environment may not be aligned with the state passed in. If the environment's current state (self.env_current_obs) is not the same as the state passed in, backtracking is needed. The basic implementation of this is rather naive, as it just resets the environment and replays the actions in the state's action_history list. Depending on the environment, there may be far more efficient ways to do so.
+        Takes in a state and action and steps the environment. Should be noted 
+        that the environment may not be aligned with the state passed in. If 
+        the environment's current state (self.env_current_obs) is not the 
+        same as the state passed in, backtracking is needed. The basic 
+        implementation of this is rather naive, as it just resets the 
+        environment and replays the actions in the state's action_history list. 
+        Depending on the environment, there may be far more efficient ways to do so.
 
-        Args:
-        - state (StateGym): the state to step from
-        - action (ActionGym): the action to take from the state
+        Parameters
+        ----------
+        state : StateGym
+            the state to step from
+        action : ActionGym
+            the action to take from the state
 
-        Returns:
-        - next_state (StateGym): the next state after taking the action
-        - aux (dict): used to pass the environment's reward to the search algorithm, which then passes it to the SearchConfig's reward function
+        Returns
+        -------
+        next_state : StateGym
+            the next state after taking the action
+        aux : dict
+            used to pass the environment's reward to the search algorithm, 
+            which then passes it to the SearchConfig's reward function
         """
 
         if self.env_current_obs != state.current_obs:
@@ -120,4 +144,7 @@ class EnvironmentGym(Environment):
         return next_state, {"env_reward": reward}
 
     def is_terminal(self, state: StateGym) -> bool:
+        """
+        Checks if environment reached terminal state
+        """
         return state.terminated
